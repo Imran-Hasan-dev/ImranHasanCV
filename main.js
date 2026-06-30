b51a7658207f292afea9bdadd7f866968a49af46
Imran Hasan <70254704+Imran-Hasan-dev@users.noreply.github.com>
2026-06-30 15:50:39 +0600

    Refactor navigation and scroll reveal functions project 1.0

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
 function myMenuFunction(){
   var menuBtn = document.getElementById("myNavMenu");
-
   if(menuBtn.className === "nav-menu"){
     menuBtn.className += " responsive";
   } else {
     menuBtn.className = "nav-menu";
   }
 }
-
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
 window.onscroll = function() {headerShadow()};
-
 function headerShadow() {
   const navHeader =document.getElementById("header");
-
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
-
     navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
     navHeader.style.height = "70px";
     navHeader.style.lineHeight = "70px";
-
   } else {
-
     navHeader.style.boxShadow = "none";
     navHeader.style.height = "90px";
     navHeader.style.lineHeight = "90px";
-
   }
 }
-
-
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
   strings : ["Consultant","Developer"],
@@ -39,8 +29,6 @@ var typingEffect = new Typed(".typedText",{
   backSpeed : 80,
   backDelay : 2000
 })
-
-
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
 const sr = ScrollReveal({
       origin: 'top',
@@ -48,7 +36,6 @@ const sr = ScrollReveal({
       duration: 2000,
       reset: true     
 })
-
 /* -- HOME -- */
 sr.reveal('.featured-text-card',{})
 sr.reveal('.featured-name',{delay: 100})
@@ -56,16 +43,11 @@ sr.reveal('.featured-text-info',{delay: 200})
 sr.reveal('.featured-text-btn',{delay: 200})
 sr.reveal('.social_icons',{delay: 200})
 sr.reveal('.featured-image',{delay: 300})
-
-
-/* -- PROJECT BOX -- */
-sr.reveal('.project-box',{interval: 200})
-
+/* -- PROJECT CARDS -- */
+sr.reveal('.project-card',{interval: 150})
 /* -- HEADINGS -- */
 sr.reveal('.top-header',{})
-
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
 origin: 'left',
@@ -73,10 +55,8 @@ distance: '80px',
 duration: 2000,
 reset: true
 })
-
 srLeft.reveal('.about-info',{delay: 100})
 srLeft.reveal('.contact-info',{delay: 100})
-
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
 origin: 'right',
@@ -84,56 +64,33 @@ distance: '80px',
 duration: 2000,
 reset: true
 })
-
 srRight.reveal('.skills-box',{delay: 100})
 srRight.reveal('.form-control',{delay: 100})
-
-
-
 /* ----- CHANGE ACTIVE LINK ----- */
-
 const sections = document.querySelectorAll('section[id]')
-
 function scrollActive() {
 const scrollY = window.scrollY;
-
 sections.forEach(current =>{
   const sectionHeight = current.offsetHeight,
       sectionTop = current.offsetTop - 50,
     sectionId = current.getAttribute('id')
-
   if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
-
       document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-
   }  else {
-
     document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-
   }
 })
 }
-
 window.addEventListener('scroll', scrollActive)
-
 function downloadFile() {
   const link = document.createElement('a');
-  link.href = 'Imran Hasan CV.pdf'; // Replace with your file path and name
-  link.download = 'Imran Hasan CV'; // This will be the default name of the downloaded file
-  link.target = '_blank'; // Opens the file in a new tab
-            link.rel = 'noopener noreferrer'; // For security reasons
+  link.href = 'Imran Hasan CV.pdf';
+  link.download = 'Imran Hasan CV';
+  link.target = '_blank';
+            link.rel = 'noopener noreferrer';
             link.click();
 }
 function sendEmail(){
-//  Email.send({
-// SecureToken : "de15985d-576a-4ea7-85f4-e35a9a052956",
-// To : 'imranhasan49.rt@gmail.com',
-// From : "kratosgow143@gmail.com",
-// Subject : "New Contact through web",
-// Body : "And this is the body"
-// }).then(
-// message => alert(message)
-// );
   Email.send({
     Host : "smtp.elasticemail.com",
     Username : "kratosgow143@gmail.com",
@@ -150,9 +107,3 @@ function goTo(){
   window.open(
     "https://mail.google.com/mail/u/0/#inbox?compose=CllgCJlDTMFkVChgfTnlxqHcDJWDdxpGqpWDKwTFLzPRkGQpnLRgddjtbTKfCwRTPgnzcsgCGBV", "_blank");
 }
-
-
-
-
-
-
